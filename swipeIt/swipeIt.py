0cd3db1cce645fa501392337727b25dff0998be8
Abul Al Arabi <68565388+abulalarabi@users.noreply.github.com>
2024-03-27 11:04:28 -0500

    Update swipeIt.py

--- swipeIt/swipeIt.py
+++ swipeIt/swipeIt.py
@@ -1,10 +1,1 @@
-from pykeyboard import PyKeyboard
-from time import sleep
-keyboard = PyKeyboard()
-
-text = "Hi, this is Arabi, typing virtually"
-
-sleep(7)
-for t in text:
-    keyboard.tap_key(t)
-    sleep(0.1)
+# ***** YOUR CODE ***** #
